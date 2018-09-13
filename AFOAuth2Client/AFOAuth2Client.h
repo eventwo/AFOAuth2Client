76ceb0bf9249bf84e23b24a77de99a084e123689
Pedro Palmero <pedropalmero@gmail.com>
2018-09-13 12:16:02 +0200

    Import AFNetworking module

--- AFOAuth2Client/AFOAuth2Client.h
+++ AFOAuth2Client/AFOAuth2Client.h
@@ -19,7 +19,7 @@
 // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
-#import "AFNetworking.h"
+#import <AFNetworking/AFNetworking.h>
 
 #ifndef _SECURITY_SECITEM_H_
 #warning Security framework not found in project, or not included in precompiled header. Keychain persistence functionality will not be available.
